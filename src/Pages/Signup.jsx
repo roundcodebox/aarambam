f105b6edaf60be90b82e8a3bcfdafa303b3a0837
sajankota <sajankota001@gmail.com>
2022-05-02 22:39:36 +0530

    feat: added the signin and signup pages

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Signup() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>This is the Signup Page</h2>
+        </Col>
+        <Col>
+          <h2>Signup</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Signup;
