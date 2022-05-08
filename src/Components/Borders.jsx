3ecfb56d84552558f20fd0a791674733f139e358
sajankota <sajankota001@gmail.com>
2022-05-08 14:35:14 +0530

    feat: removed the react-bootstrap components from the borders page

--- src/Components/Borders.jsx
+++ src/Components/Borders.jsx
@@ -1,140 +1,68 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 
-function Borders() {
+function Border() {
   return (
-    <Container>
-      <Row>
-        <h4>Borders Colors</h4>
-        <Col sm={3} className=" border border-primary m-4">
-          <p>Primary Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-secondary m-4">
-          <p>Secondary Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-success m-4">
-          <p>Success Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-danger m-4">
-          <p>Danger Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-warning m-4">
-          <p>Warning Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-info m-4">
-          <p>Info Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-light m-4">
-          <p>Light Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-dark m-4">
-          <p>Dark Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-white m-4">
-          <p>White Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <h4>Border Width</h4>
-        <Col sm={3} className="border border-1 m-1">
-          <p>Success Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-2 m-1">
-          <p>Success Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-3 m-1">
-          <p>Success Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-4 m-1">
-          <p>Success Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-        <Col sm={3} className="border border-5 m-1">
-          <p>Success Borders</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-            laboriosam eum esse aliquam corrupti, enim, in non sed magni dolorum
-            vitae explicabo, illum dicta necessitatibus maiores! Obcaecati ipsam
-            perferendis illo.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <div className="container px-4 py-4">
+      <div className="row g-5 py-5">
+        <div className="col-sm-12 col-lg-12">
+          <div className="row g-5 py-4">
+            <h4>Border Colors</h4>
+            <div className="col bg-light border border-primary m-2 sm-4">
+              <p>Primary Border</p>
+            </div>
+            <div
+              sm={3}
+              className="col bg-light border border-secondary m-2 sm-4"
+            >
+              <p>Secondary Border</p>
+            </div>
+            <div sm={3} className="col bg-light border border-success m-2 sm-4">
+              <p>Success Border</p>
+            </div>
+          </div>
+          <div className="row g-5 py-4">
+            <div sm={3} className=" col bg-light border border-danger m-2 sm-4">
+              <p>Danger Border</p>
+            </div>
+            <div sm={3} className="col bg-light border border-warning m-2 sm-4">
+              <p>Warning Border</p>
+            </div>
+            <div sm={3} className="col bg-light border border-info m-2 sm-4">
+              <p>Info Border</p>
+            </div>
+          </div>
+          <div className="row g-5 py-4">
+            <div sm={3} className="col bg-light border border-light m-2 sm-4">
+              <p>Light Border</p>
+            </div>
+            <div sm={3} className="col bg-light border border-dark m-2 sm-4">
+              <p>Dark Border</p>
+            </div>
+            <div sm={3} className="col bg-light border border-white m-2 sm-4">
+              <p>White Border</p>
+            </div>
+          </div>
+          <div className="row g-5 py-4">
+            <h4>Border Width</h4>
+            <div sm={3} className="col bg-light border border-1 m-1 sm=3">
+              <p>Border width of 1</p>
+            </div>
+            <div sm={3} className="col bg-light border border-2 m-1 sm=3">
+              <p>Border width of 2</p>
+            </div>
+            <div sm={3} className="col bg-light border border-3 m-1 sm=3">
+              <p>Border width of 3</p>
+            </div>
+            <div sm={3} className="col bg-light border border-4 m-1 sm=3">
+              <p>Border width of 4</p>
+            </div>
+            <div sm={3} className="col bg-light border border-5 m-1">
+              <p>Border width of 4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-export default Borders;
+export default Border;
